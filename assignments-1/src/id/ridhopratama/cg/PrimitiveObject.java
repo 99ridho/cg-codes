3f2cd315e37f902ccbbaed8fc66d13fadcb00a3e
M. Ridho K. Pratama <99ridho@gmail.com>
2016-03-17 17:02:32 +0700

    gambar lingkaran dan persegi. Kurang diisi pattern coy :(

--- assignments-1/src/id/ridhopratama/cg/PrimitiveObject.java
+++ assignments-1/src/id/ridhopratama/cg/PrimitiveObject.java
@@ -4,7 +4,7 @@ import org.lwjgl.opengl.*;
 import org.lwjgl.LWJGLException;
 import static org.lwjgl.opengl.GL11.*;
 
-// Soal 1C
+// Soal 1A, 1B, 1C
 public class PrimitiveObject {
 
     private void start() {
@@ -40,50 +40,93 @@ public class PrimitiveObject {
     }
     
     private void draw() {
-        // gambar segi 4
+        
+        // gambar rumah
         glPushMatrix();
-            glTranslatef(120, 100, 0);
-            glBegin(GL_QUADS);
-                glColor3f(0.44f, 0.188f, 0.627f);
-                glVertex2f(120, 100);
-                glVertex2f(100 + 185, 100);
-                glVertex2f(100 + 185, 100 + 100);
-                glVertex2f(120, 100 + 100);
-            glEnd();
-        glPopMatrix();
+            glTranslatef(-100, 100, 0);
+            
+            // gambar segi 4
+            glPushMatrix();
+                glTranslatef(120, 100, 0);
+                glBegin(GL_QUADS);
+                    glColor3f(0.44f, 0.188f, 0.627f);
+                    glVertex2f(120, 100);
+                    glVertex2f(100 + 185, 100);
+                    glVertex2f(100 + 185, 100 + 100);
+                    glVertex2f(120, 100 + 100);
+                glEnd();
+            glPopMatrix();
 
-        //gambar cerobong asap
-        glPushMatrix();
-            glTranslatef(250, 180, 0);
-            glBegin(GL_QUADS);
-                glColor3f(0.5f, 0.5f, 0.5f);
-                glVertex2f(120, 120);
-                glVertex2f(150, 120);
-                glVertex2f(150, 200);
-                glVertex2f(120, 200);
-            glEnd();
-        glPopMatrix();
+            //gambar cerobong asap
+            glPushMatrix();
+                glTranslatef(250, 180, 0);
+                glBegin(GL_QUADS);
+                    glColor3f(0.5f, 0.5f, 0.5f);
+                    glVertex2f(120, 120);
+                    glVertex2f(150, 120);
+                    glVertex2f(150, 200);
+                    glVertex2f(120, 200);
+                glEnd();
+            glPopMatrix();
 
-        // gambar segitiga buat atap
-        glPushMatrix();
-            glTranslatef(225, 250, 0);
-            glBegin(GL_TRIANGLES);
-                glColor3f(0.6f, 0.4f, 0.2f);
-                glVertex2f(-10, 50);
-                glVertex2f(205, 50);
-                glVertex2f(100, 135);
-            glEnd();  
+            // gambar segitiga buat atap
+            glPushMatrix();
+                glTranslatef(225, 250, 0);
+                glBegin(GL_TRIANGLES);
+                    glColor3f(0.6f, 0.4f, 0.2f);
+                    glVertex2f(-10, 50);
+                    glVertex2f(205, 50);
+                    glVertex2f(100, 135);
+                glEnd();  
+            glPopMatrix();
+
+            // gambar pintu
+            glPushMatrix();
+                glTranslatef(210, 100, 0);
+                glBegin(GL_QUADS);
+                    glColor3f(0.0f, 0.0f, 0.0f);
+                    glVertex2f(100, 100);
+                    glVertex2f(130, 100);
+                    glVertex2f(130, 130);
+                    glVertex2f(100, 130);
+                glEnd();
+            glPopMatrix();
         glPopMatrix();
         
-        // gambar pintu
+        // gambar kotak bolong
         glPushMatrix();
-            glTranslatef(210, 100, 0);
+            glTranslatef(450, 250, 0);
             glBegin(GL_QUADS);
-                glColor3f(0.0f, 0.0f, 0.0f);
+                glColor3f(1.0f, 1.0f, 1.0f);
                 glVertex2f(100, 100);
-                glVertex2f(130, 100);
-                glVertex2f(130, 130);
-                glVertex2f(100, 130);
+                glVertex2f(200, 100);
+                glVertex2f(200, 200);
+                glVertex2f(100, 200);
+            glEnd();
+            
+            glLineWidth(2.0f);
+            glBegin(GL_LINE_STRIP);
+                glColor3f(0.9f, 0.73f, 0.52f);
+                glVertex2f(100, 100);
+                glVertex2f(200, 100);
+                glVertex2f(200, 200);
+                glVertex2f(100, 200);
+                glVertex2f(100, 100);
+            glEnd();
+        glPopMatrix();
+        
+        // gambar lingkaran
+        glPushMatrix();
+            glTranslatef(500, 250, 0);
+            glScalef(60, 60, 0);
+            glBegin(GL_LINE_LOOP);
+                glColor3f(1.0f, 0.0f, 0.0f);
+                for (int i = 0 ; i <= 300 ; i++) {
+                    double angle = 2 * Math.PI * i / 300;
+                    double x = Math.cos(angle);
+                    double y = Math.sin(angle);
+                    glVertex2d(x, y);
+                }
             glEnd();
         glPopMatrix();
     }
