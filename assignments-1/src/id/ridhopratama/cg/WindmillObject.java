1ebeffc00bf89f40314e4571d3d52b267087ac32
M. Ridho K. Pratama <99ridho@gmail.com>
2016-03-18 18:34:00 +0700

    Windmill again. Rotate gak di local axis :(

--- assignments-1/src/id/ridhopratama/cg/WindmillObject.java
+++ assignments-1/src/id/ridhopratama/cg/WindmillObject.java
@@ -22,7 +22,8 @@ public class WindmillObject {
         // init OpenGL
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, 800, 0, 600, -1, 1);
+        //glOrtho(0, 800, 0, 600, -1, 1);
+        glOrtho(-200, 200, -150, 150, -1, 1);
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
@@ -44,48 +45,57 @@ public class WindmillObject {
 
     private void draw() {
         
+        // titik pusat
+        glPointSize(6);
+        glBegin(GL_POINTS);
+            glColor3f(0.0f, 0.0f, 0.0f);
+            glVertex2f(0, 0);
+        glEnd();
+        
         // gambar kaki kincir angin
         glPushMatrix();
-            glTranslatef(300, 20, 0);
-            glScalef(1.5f, 1.5f, 0);
+            glTranslatef(-60, -60, 0);
+            glScalef(0.5f, 0.5f, 0);
             glBegin(GL_TRIANGLES);
                 glColor3f(1.0f, 0.56f, 0.0f);
-                glVertex2f(5, 30);
+                glVertex2f(-20, -10);
                 glColor3f(0.0f, 0.0f, 1.0f);
-                glVertex2f(85, 30);
+                glVertex2f(100, -10);
                 glColor3f(0.0f, 1.0f, 0.0f);
                 glVertex2f(45, 200);
             glEnd();
         glPopMatrix();
         
-        //glPushMatrix();
+        glPushMatrix();
+            glTranslatef(-78, -75, 0);
+            glRotatef(animStep+=0.1f, 0.0f, 0.0f, 1.0f);
             glPushMatrix();
-                glTranslatef(-120, -60, -0);
-                glPushMatrix();
-                    glTranslatef(350, 180, 0);
-                    glBegin(GL_QUADS);
-                        glColor3f(1.0f, 0.0f, 0.0f);
-                        glVertex2f(120, 120);
-                        glVertex2f(150, 120);
-                        glVertex2f(150, 275);
-                        glVertex2f(120, 275);
-                    glEnd();
-                glPopMatrix();
-
-                glPushMatrix();
-                    glTranslatef(285, 515, 0);
-                    glRotatef(-90, 0.0f, 0.0f, 1.0f);
-                    glBegin(GL_QUADS);
-                        glColor3f(1.0f, 0.5f, 0.0f);
-                        glVertex2f(120, 120);
-                        glVertex2f(150, 120);
-                        glVertex2f(150, 275);
-                        glVertex2f(120, 275);
-                    glEnd();
-                glPopMatrix();
+                //glTranslatef(350, 180, 0);
+                glScalef(3.0f, 5.0f, 0);
+                glBegin(GL_QUADS);
+                    glColor3f(1.0f, 0.0f, 0.0f);
+                    glVertex2f(12, 12);
+                    glVertex2f(15, 12);
+                    glVertex2f(15, 27);
+                    glVertex2f(12, 27);
+                glEnd();
             glPopMatrix();
-        //glPopMatrix();
-        
+
+            glPushMatrix();
+                //glTranslatef(350, 180, 0);
+                glTranslatef(-3, 28, 0);
+                glRotatef(90.0f, 0.0f, 0.0f, 1.0f);
+                glScalef(3.0f, 5.0f, 0);
+                glTranslatef(3, -28, 0);
+                glBegin(GL_QUADS);
+                    glColor3f(1.0f, 0.5f, 0.0f);
+                    glVertex2f(20, 12);
+                    glVertex2f(23, 12);
+                    glVertex2f(23, 27);
+                    glVertex2f(20, 27);
+                glEnd();
+            glPopMatrix();
+        glPopMatrix();
         
     }
 
